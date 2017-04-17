f06924464cdb46f92b444f861812a041a5e9b25b
Yuqi <ywan418@student.monash.edu>
2017-04-17 11:07:37 +1000

    Fragment update

--- app/src/main/java/yuqiwang/automobilemodificationconfigurator/Previewer.java
+++ app/src/main/java/yuqiwang/automobilemodificationconfigurator/Previewer.java
@@ -1,6 +1,7 @@
 package yuqiwang.automobilemodificationconfigurator;
 
 import android.app.ActionBar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
@@ -8,30 +9,11 @@ import android.view.ViewGroup;
 import org.rajawali3d.surface.IRajawaliSurface;
 import org.rajawali3d.surface.RajawaliSurfaceView;
 
-public class Previewer extends AppCompatActivity {
-
-    Renderer renderer;
+public class Previewer extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final RajawaliSurfaceView surface = new RajawaliSurfaceView(this);
-        surface.setFrameRate(60.0);
-        surface.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
-
-        // Add mSurface to your root view
-        addContentView(surface, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
-
-        renderer = new Renderer(this);
-        surface.setSurfaceRenderer(renderer);
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        renderer.stopRendering();
-        super.onBackPressed();
+        setContentView(R.layout.activity_previewer);
     }
 }
