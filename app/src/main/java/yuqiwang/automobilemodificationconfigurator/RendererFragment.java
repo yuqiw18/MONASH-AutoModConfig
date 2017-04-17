2c13d37c006f6d7f6b1d1e2fe99aeca04cf6c008
Yuqi <ywan418@student.monash.edu>
2017-04-17 18:54:34 +1000

    .

--- app/src/main/java/yuqiwang/automobilemodificationconfigurator/RendererFragment.java
+++ app/src/main/java/yuqiwang/automobilemodificationconfigurator/RendererFragment.java
@@ -1,10 +1,7 @@
 package yuqiwang.automobilemodificationconfigurator;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,30 +11,31 @@ import org.rajawali3d.surface.RajawaliSurfaceView;
 
 public class RendererFragment extends Fragment {
 
-
     Renderer renderer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_renderer, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        final RajawaliSurfaceView surface = new RajawaliSurfaceView(this.getContext());
+        View view = inflater.inflate(R.layout.fragment_renderer, container, false);
+        final RajawaliSurfaceView surface = (RajawaliSurfaceView) view.findViewById(R.id.rajawali_surface);
         surface.setFrameRate(60.0);
         surface.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
-
-        // Add mSurface to your root view
-        //(surface, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
-
         renderer = new Renderer(this.getContext());
         surface.setSurfaceRenderer(renderer);
 
+        return view;
+        //return inflater.inflate(R.layout.fragment_renderer, container, false);
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        renderer.stopRendering();
+        super.onDestroy();
+    }
 }
