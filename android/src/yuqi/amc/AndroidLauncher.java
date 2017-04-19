0a388b4e4b03c931c99394d301a6eb133c0cf3de
Yuqi <ywan418@student.monash.edu>
2017-04-19 18:40:47 +1000

    .

--- android/src/yuqi/amc/AndroidLauncher.java
+++ android/src/yuqi/amc/AndroidLauncher.java
@@ -1,16 +1,16 @@
-package yuqi.amc;
-
-import android.os.Bundle;
-
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import yuqi.amc.main;
-
-public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new main(), config);
-	}
-}
+//package yuqi.amc;
+//
+//import android.os.Bundle;
+//
+//import com.badlogic.gdx.backends.android.AndroidApplication;
+//import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+//import yuqi.amc.main;
+//
+//public class AndroidLauncher extends AndroidApplication {
+////	@Override
+////	protected void onCreate (Bundle savedInstanceState) {
+////		super.onCreate(savedInstanceState);
+////		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+////		initialize(new main(), config);
+////	}
+//}
