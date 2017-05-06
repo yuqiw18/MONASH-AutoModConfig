0797ba87ce23863ab5915938b0c3de9fc995789b
Yuqi Wang <ywan418@student.monash.edu>
2017-05-07 01:35:50 +1000

    Major Update

--- android/src/yuqi/amc/JsonData/Customer.java
+++ android/src/yuqi/amc/JsonData/Customer.java
@@ -1,14 +1,117 @@
 package yuqi.amc.JsonData;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by ClayW on 6/05/2017.
  */
 
 public class Customer {
 
+    private String name;
+    private String password;
+    private String email;
+    private String address;
+    private String suburb;
+    private int postcode;
+    private String state;
+    private String country;
 
+    public Customer(){}
 
+    public Customer(String name, String password, String email, String address, String suburb, int postcode, String state, String country){
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.address = address;
+        this.suburb = suburb;
+        this.postcode = postcode;
+        this.state = state;
+        this.country = country;
+    }
 
+    public static Customer jsonToCustomer(String json){
+        Customer customer = new Customer();
+        try {
+            JSONArray jsonArray = new JSONArray(json);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            customer.setName(jsonObject.getString("CUSTOMER_NAME"));
+            customer.setEmail(jsonObject.getString("CUSTOMER_EMAIL"));
+            customer.setPassword(jsonObject.getString("CUSTOMER_PASSWORD"));
+            customer.setAddress(jsonObject.getString("CUSTOMER_ADDRESS"));
+            customer.setSuburb(jsonObject.getString("CUSTOMER_SUBURB"));
+            customer.setPostcode(jsonObject.getInt("CUSTOMER_POSTCODE"));
+            customer.setState(jsonObject.getString("CUSTOMER_STATE"));
+            customer.setCountry(jsonObject.getString("CUSTOMER_COUNTRY"));
+        }catch (Exception e){
+            e.printStackTrace();
+            customer = null;
+        }
+        return customer;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
--- android/src/yuqi/amc/Login.java
+++ android/src/yuqi/amc/Login.java
@@ -2,7 +2,9 @@ package yuqi.amc;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -12,7 +14,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import yuqi.amc.JsonData.Customer;
+
 public class Login extends AppCompatActivity implements OnClickListener {
+
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,40 +30,44 @@ public class Login extends AppCompatActivity implements OnClickListener {
         setTitle("AMC Customer Login");
         Button btnLogin = (Button)findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
     }
-
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnLogin){
             new validateLogin().execute();
         }else if (v.getId() == R.id.labelForgetPassword){
+            // Display a dialog for customers to enter their email address
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             View dialogView = getLayoutInflater().inflate(R.layout.dialog_forget_password, null);
-            final EditText requrestEmail = (EditText)dialogView.findViewById(R.id.inputForgetPasswordEmail);
+            final EditText requestEmail = (EditText)dialogView.findViewById(R.id.inputForgetPasswordEmail);
             Button btnSubmitRequest = (Button)dialogView.findViewById(R.id.btnForgetPasswordRequest);
+            builder.setView(dialogView);
+            final AlertDialog alertDialog = builder.create();
             btnSubmitRequest.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String input = requrestEmail.getText().toString().trim();
+                    // Validate email address
+                    String input = requestEmail.getText().toString().trim();
                     if (!input.isEmpty()){
+                        // If it matches the pattern xx@xx.xx
                         if(!Patterns.EMAIL_ADDRESS.matcher(input).matches()){
                             Toast.makeText(getBaseContext(), getString(R.string.dialog_forget_password_invalid), Toast.LENGTH_LONG).show();
                         }else {
-
+                            // Send request to server
+                            new requestPassword().execute(input);
+                            alertDialog.dismiss();
                         }
                     }else {
                         Toast.makeText(getBaseContext(), getString(R.string.dialog_forget_password_validate_empty), Toast.LENGTH_LONG).show();
                     }
                 }
             });
-            builder.setView(dialogView);
-            builder.create().show();
+            alertDialog.show();
         }else if (v.getId() == R.id.labelSignUp){
-
-
+            startActivity(new Intent(this, Register.class));
         }
-
     }
 
     private class validateLogin extends AsyncTask<String, Void, String>{
@@ -66,12 +79,28 @@ public class Login extends AppCompatActivity implements OnClickListener {
         @Override
         protected void onPostExecute(String result) {
             if (result!=null && !result.isEmpty()){
-                Intent intent = new Intent(Login.this, MainMenu.class);
-                // Bring MainMenu to the top stack. By doing this, clicking back button will not bring user to the login screen any more.
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("JsonArray", result);
-                startActivity(intent);
+                Customer customer = Customer.jsonToCustomer(result);
+                if (customer!=null){
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("name",customer.getName());
+                    editor.putString("email",customer.getEmail());
+                    editor.putString("password",customer.getPassword());
+                    editor.putString("address",customer.getAddress());
+                    editor.putString("suburb",customer.getSuburb());
+                    editor.putInt("postcode",customer.getPostcode());
+                    editor.putString("state",customer.getState());
+                    editor.putString("country",customer.getCountry());
+                    editor.putBoolean("isSignedIn",true);
+                    editor.commit();
+
+                    Intent intent = new Intent(Login.this, MainMenu.class);
+                    // Bring MainMenu to the top stack. By doing this, clicking back button will not bring user to the login screen any more.
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getBaseContext(), getString(R.string.ui_message_unknown_error), Toast.LENGTH_LONG).show();
+                }
             }else{
                 Toast.makeText(getBaseContext(), getString(R.string.login_failed), Toast.LENGTH_LONG).show();
             }
@@ -81,7 +110,7 @@ public class Login extends AppCompatActivity implements OnClickListener {
     private class requestPassword extends AsyncTask<String,Void,Void>{
         @Override
         protected Void doInBackground(String... params) {
-            RestClient.requestData("",params);
+            RestClient.requestData("request.password",params);
             return null;
         }
 
