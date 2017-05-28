cebf14b7bd2f7c495513cf6bad68bc5e31a5a867
Yuqi <ywan418@student.monash.edu>
2017-05-28 23:39:56 +1000

    Test Setup

---
+++ android/test/yuqi/amc/LoginTest.java
@@ -0,0 +1,13 @@
+package yuqi.amc;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by yuqi on 28/5/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class LoginTest {
+}
